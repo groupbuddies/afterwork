6208776a0691a38d1994807446cb865991a0b815
Mário abreu <mjm.abreu@gmail.com>
2015-03-03 14:00:46 +0000

    removed email field from users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150302134842) do
+ActiveRecord::Schema.define(version: 20150303134404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,6 @@ ActiveRecord::Schema.define(version: 20150302134842) do
 
   create_table "users", force: :cascade do |t|
     t.string "name",     limit: 60, null: false
-    t.string "email"
     t.string "location"
     t.string "gender"
   end
