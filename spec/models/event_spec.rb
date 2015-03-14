9c0cf8b18341edd4db936a32440b17e1d5b9d5bf
Mário abreu <mjm.abreu@gmail.com>
2015-03-14 15:44:51 +0000

    Removed useless check on current_user. Chenged the test on notify_users.

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -3,13 +3,13 @@ require 'spec_helper'
 describe Event do
   context '#users_available' do
     context 'A event has users available' do
-      it 'returns a list with a user' do
+      it 'calls the action notify_users' do
         stub_request(:any, 'https://api.twitter.com/1.1/statuses/update.json')
         user = create(:user, interest_list: 'Ruby')
-        Availability.create(week_day: 5, start_time: DateTime.new(2015, 03, 06, 20, 00), end_time: DateTime.new(2015, 03, 06, 23, 30), user_id: user.id)
-        event = Event.create(name: 'Random Event', location: 'Braga', start_date: DateTime.new(2015, 03, 06, 20, 30), end_date: DateTime.new(2015, 03, 06, 21, 00), owner: user.id, interest_list: 'Ruby')
+        Availability.create(week_day: 5, start_time: DateTime.new(2015, 03, 06, 20, 00), user_id: user.id)
+        event = Event.create(name: 'Random Event', location: 'Braga', start_date: DateTime.new(2015, 03, 06, 20, 30), owner: user.id, interest_list: 'Ruby')
 
-        allow(event).to reveice(:notify_users)
+        allow(event).to receive(:notify_users)
         event.notify_users
 
         expect(event).to have_received(:notify_users)
